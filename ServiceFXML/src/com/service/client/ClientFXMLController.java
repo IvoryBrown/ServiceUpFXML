e76aad63dab5cdebc80d8d7837e7892ffbe853c7
SzervizIvo <paradisho@gmail.com>
2017-12-12 17:01:48 +0100

    init

---
+++ ServiceFXML/src/com/service/client/ClientFXMLController.java
@@ -0,0 +1,63 @@
+package com.service.client;
+
+import java.net.URL; 
+import java.util.ResourceBundle;
+
+import com.service.main.ServiceController;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ClientFXMLController extends ServiceController implements Initializable {
+
+	@FXML
+	private ComboBox<String> cmbClientInputCounty; 
+	@FXML
+	private TextField txtClientInputClientName;
+	@FXML
+	private Label txtClientInputID, txtClientInputNumber; 
+	@FXML
+	private Button btnClientNewClient;
+
+	private final String COUNTRYCOUNTIES[] = { "Bács-Kiskun", "Baranya", "Békés", "Borsod-Abaúj-Zemplén", "Csongrád",
+			"Fejér", "Gyõr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", "Komárom-Esztergom", "Nógrád",
+			"Pest", "Somogy", "Szabolcs-Szatmár-Bereg", "Tolna", "Vas", "Veszprém", "Zala" };
+
+	private void setComboxAll() {
+		cmbClientInputCounty.getItems().addAll(COUNTRYCOUNTIES);
+		btnClientNewClient.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				btnClientNewclient();
+				
+			}
+		});
+	}
+
+	private boolean clientInput() {
+		if(txtClientInputClientName.getText().trim().isEmpty()) {
+			return false; 
+		}
+		return true;
+	}
+	
+	
+	private void btnClientNewclient(){
+		if (clientInput()) {
+			txtClientInputID.setText("Yes");
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		setMenuData();
+		setComboxAll();
+	}
+}
---
+++ ServiceFXML/src/com/service/main/Main.java
@@ -0,0 +1,28 @@
+package com.service.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/com/service/main/ServiceFX.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setWidth(1350);
+			primaryStage.setHeight(710);
+			primaryStage.setTitle("Szerviz1");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
