6c4682157babe751d9320e0abae1b6e1f42d4995
SzervizIvo <paradisho@gmail.com>
2018-01-11 21:11:01 +0100

    ini

--- ServiceFXML/src/com/service/stock/Stock.java
+++ ServiceFXML/src/com/service/stock/Stock.java
@@ -1,20 +1,24 @@
 package com.service.stock;
 
-import javafx.beans.property.SimpleSetProperty;
+import javafx.beans.property.SimpleStringProperty;
 
 public class Stock {
 
-	private final SimpleSetProperty device;
-	private final  SimpleSetProperty device1;
+	private final SimpleStringProperty device;
+	private final  SimpleStringProperty device1;
+	
+	public Stock(SimpleStringProperty device, SimpleStringProperty device1) {
+		this.device = device;
+		this.device1 = device1;
+	}
+	public String  getDevice() {
+		return device.get();
+	}
+	public SimpleStringProperty getDevice1() {
+		return device1;
+	}
 	 
-	public Stock(SimpleSetProperty device, SimpleSetProperty device1) {
-		this.device = new SimpleSetProperty(device);
-		this.device1 = new SimpleSetProperty(device1);
-	}
-
-	public SimpleSetProperty getDevice() {
-		return  device.get();
-	}
+	
 
 	
 
