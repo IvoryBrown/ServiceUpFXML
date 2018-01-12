2b1dceca3d382bcbe3b8244daa35029cdd8013dd
SzervizIvo <paradisho@gmail.com>
2018-01-12 06:27:04 +0100

    init

--- ServiceFXML/src/com/service/stock/Stock.java
+++ ServiceFXML/src/com/service/stock/Stock.java
@@ -4,25 +4,49 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class Stock {
 
-	private final SimpleStringProperty device;
-	private final  SimpleStringProperty device1;
-	
-	public Stock(SimpleStringProperty device, SimpleStringProperty device1) {
-		this.device = device;
-		this.device1 = device1;
-	}
-	public String  getDevice() {
-		return device.get();
-	}
-	public SimpleStringProperty getDevice1() {
-		return device1;
-	}
-	 
-	
+	private final SimpleStringProperty stockId;
+	private final SimpleStringProperty stockDevice;
+	private final SimpleStringProperty stockDescription;
 
-	
+	public Stock() {
+		this.stockId = new SimpleStringProperty("");
+		this.stockDevice = new SimpleStringProperty("");
+		this.stockDescription = new SimpleStringProperty("");
+	}
 
-	
+	public Stock(String sDevice, String sDescription) {
+		this.stockId = new SimpleStringProperty("");
+		this.stockDevice = new SimpleStringProperty(sDevice);
+		this.stockDescription = new SimpleStringProperty(sDescription);
+	}
 
-	
+	public Stock(Integer sId, String sDevice, String sDescription) {
+		this.stockDevice = new SimpleStringProperty(sDevice);
+		this.stockDescription = new SimpleStringProperty(sDescription);
+		this.stockId = new SimpleStringProperty(String.valueOf(sId));
+	}
+
+	public String getStockId() {
+		return stockId.get();
+	}
+
+	public void setStockId(String sId) {
+		stockId.set(sId);
+	}
+
+	public String getStockDevice() {
+		return stockDevice.get();
+	}
+
+	public void setStockDevice(String sDevice) {
+		stockDevice.set(sDevice);
+	}
+
+	public String getStockDescription() {
+		return stockDescription.get();
+	}
+
+	public void setStockDescription(String sDescription) {
+		stockDescription.set(sDescription);
+	}
 }
