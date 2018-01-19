d967b50ea2bfa4e9053c77831c36a20abb94b8bd
SzervizIvo <paradisho@gmail.com>
2018-01-19 15:10:23 +0100

    init

--- ServiceFXML/src/com/service/stock/Stock.java
+++ ServiceFXML/src/com/service/stock/Stock.java
@@ -1,64 +1,81 @@
 package com.service.stock;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Stock {
 
-	private final SimpleStringProperty stockDeviceId;
+	private final SimpleIntegerProperty stockDeviceId;
 	private final SimpleStringProperty stockDeviceName;
-	private final SimpleStringProperty stockDeviceQuantity;
+	private final SimpleStringProperty stockDeviceDate;
+	private final SimpleStringProperty stockDeviceSalesDate;
+	private final SimpleIntegerProperty stockDeviceQuantity;
 	private final SimpleStringProperty stockDeviceDescription;
 
 	public Stock() {
-		this.stockDeviceId = new SimpleStringProperty("");
+		this.stockDeviceId = new SimpleIntegerProperty();
 		this.stockDeviceName = new SimpleStringProperty("");
-		this.stockDeviceQuantity = new SimpleStringProperty("");
+		this.stockDeviceDate = new SimpleStringProperty("");
+		this.stockDeviceSalesDate = new SimpleStringProperty("");
+		this.stockDeviceQuantity = new SimpleIntegerProperty();
 		this.stockDeviceDescription = new SimpleStringProperty("");
 	}
 
-	public Stock(String sDeviceName, String sDeviceQuantity, String sDeviceDescription) {
-		this.stockDeviceId = new SimpleStringProperty("");
+	public Stock(Integer sDeviceId, String sDeviceName, String sDeviceDate, String sDeviceSalesDate,
+			Integer sDeviceQuantity, String sDeviceDescription) {
+		this.stockDeviceId = new SimpleIntegerProperty(sDeviceId);
 		this.stockDeviceName = new SimpleStringProperty(sDeviceName);
-		this.stockDeviceQuantity = new SimpleStringProperty(sDeviceQuantity);
+		this.stockDeviceDate = new SimpleStringProperty(sDeviceDate);
+		this.stockDeviceSalesDate = new SimpleStringProperty(sDeviceSalesDate);
+		this.stockDeviceQuantity = new SimpleIntegerProperty(sDeviceQuantity);
 		this.stockDeviceDescription = new SimpleStringProperty(sDeviceDescription);
 	}
 
-	public Stock(Integer sDeviceId, String sDeviceName, String sDeviceQuantity, String sDeviceDescription) {
-		this.stockDeviceName = new SimpleStringProperty(sDeviceName);
-		this.stockDeviceQuantity = new SimpleStringProperty(sDeviceQuantity);
-		this.stockDeviceDescription = new SimpleStringProperty(sDeviceDescription);
-		this.stockDeviceId = new SimpleStringProperty(String.valueOf(sDeviceId));
-	}
-
-	public String getStockId() {
+	public Integer getStockDeviceId() {
 		return stockDeviceId.get();
 	}
 
-	public void setStockId(String sDeviceId) {
+	public void setStockDeviceId(Integer sDeviceId) {
 		stockDeviceId.set(sDeviceId);
 	}
 
-	public String getStockDeviceQuantity() {
+	public String getStockDeviceName() {
 		return stockDeviceName.get();
 	}
 
-	public void setStockDeviceQuantity(String sDeviceName) {
+	public void setStockDeviceName(String sDeviceName) {
 		stockDeviceName.set(sDeviceName);
 	}
 
-	public String getStockDevice() {
+	public String getStockDeviceDate() {
+		return stockDeviceDate.get();
+	}
+
+	public void getStockDeviceDate(String sDeviceDate) {
+		stockDeviceDate.set(sDeviceDate);
+	}
+
+	public String getStockDeviceSalesDate() {
+		return stockDeviceSalesDate.get();
+	}
+
+	public void setStockDeviceSalesDate(String sDeviceSalesDate) {
+		stockDeviceSalesDate.set(sDeviceSalesDate);
+	}
+
+	public Integer getStockDeviceQuantity() {
 		return stockDeviceQuantity.get();
 	}
 
-	public void setStockDevice(String sDeviceQuantity) {
+	public void setStockDeviceQuantity(Integer sDeviceQuantity) {
 		stockDeviceQuantity.set(sDeviceQuantity);
 	}
 
-	public String getStockDescription() {
+	public String getStockDeviceDescription() {
 		return stockDeviceDescription.get();
 	}
 
-	public void setStockDescription(String sDeviceDescription) {
+	public void setStockDeviceDescription(String sDeviceDescription) {
 		stockDeviceDescription.set(sDeviceDescription);
 	}
 }
