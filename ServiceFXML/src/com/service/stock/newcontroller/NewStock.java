420591bed3c3de29cdf45132a2c6ad9c3dfb2134
SzervizIvo <paradisho@gmail.com>
2018-01-26 20:33:01 +0100

    init

--- ServiceFXML/src/com/service/stock/newcontroller/NewStock.java
+++ ServiceFXML/src/com/service/stock/newcontroller/NewStock.java
@@ -34,9 +34,8 @@ public class NewStock implements Initializable {
 			stockDeviceDescription.setStyle("-fx-prompt-text-fill: red");
 		}
 		if (stockDeviceDate.getValue()==null) {
-			stockDeviceDate.setPromptText("!!!!");
+			stockDeviceDate.setStyle("-fx-prompt-text-fill: red");
 		}
-		
 		if (stockDeviceName.getText().trim().isEmpty() || stockDeviceQuantity.getText().trim().isEmpty()
 				|| stockDeviceDescription.getText().trim().isEmpty()|| stockDeviceDate.getValue()==null) {
 			return false;
@@ -58,10 +57,15 @@ public class NewStock implements Initializable {
 				insertStock.setString(1, stockDeviceName.getText());
 				insertStock.setString(2, ((TextField)stockDeviceDate.getEditor()).getText());
 				insertStock.setString(3, ((TextField)stockDeviceSalesDate.getEditor()).getText());
-				insertStock.setString(4, stockDeviceQuantity.getText());
+				try {
+				insertStock.setInt(4, Integer.parseInt(stockDeviceQuantity.getText()));
+				} catch (NumberFormatException e) {
+					
+					ShowInfo.errorInfoMessengeException("HIBA", "Nem megfelelő Egység!", e.getMessage());
+				}
 				insertStock.setString(5, stockDeviceDescription.getText());
 				insertStock.executeUpdate();
-				stockFXMLController.setStockTableData();
+				
 			} catch (SQLException e) {
 				ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", e.getMessage());
 			}
@@ -70,8 +74,7 @@ public class NewStock implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		stockDeviceDate.setStyle("-fx-font-size: 15px");
-		stockDeviceSalesDate.setStyle("-fx-font-size: 15px");
+		
 	}
 
 }
