1d65173435cd4a466c4511077103e937eb4fe974
SzervizIvo <paradisho@gmail.com>
2018-02-01 11:49:36 +0100

    Init

---
+++ ServiceFXML/src/com/service/stock/filteringdb/StockFillteringDB.java
@@ -0,0 +1,89 @@
+package com.service.stock.filteringdb;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.service.setting.database.DataBaseConnect;
+import com.service.setting.showinfo.ShowInfo;
+import com.service.stock.Stock;
+
+public class StockFillteringDB {
+
+	public static ArrayList<Stock> getAllStock() {
+		String sql = "SELECT * FROM `raktar`";
+		ArrayList<Stock> device = null;
+
+		Statement createStatement = null;
+		ResultSet rs = null;
+		Connection con = DataBaseConnect.getConnection();
+		try {
+			createStatement = con.createStatement();
+			rs = createStatement.executeQuery(sql);
+			device = new ArrayList<>();
+			while (rs.next()) {
+				Stock actualStock = new Stock(rs.getInt("category_id"), rs.getString("eszkoznev"),
+						rs.getString("kelte"), rs.getString("eladas_kelte"), rs.getInt("mennyiseg"),
+						rs.getString("leiras"));
+				device.add(actualStock);
+			}
+		} catch (SQLException e) {
+			ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", e.getMessage());
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (createStatement != null) {
+					createStatement.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", e.getMessage());
+			}
+		}
+		return device;
+	}
+
+	public static ArrayList<Stock> getStockNameFiltering(String stockDeviceNameFilteringTxt) {
+		String sql = "SELECT * FROM `raktar` WHERE CONCAT (`" + "eszkoznev" + "`) LIKE '%" + stockDeviceNameFilteringTxt
+				+ "%'";
+		ArrayList<Stock> device = null;
+
+		Statement createStatement = null;
+		ResultSet rs = null;
+		Connection con = DataBaseConnect.getConnection();
+		try {
+			createStatement = con.createStatement();
+			rs = createStatement.executeQuery(sql);
+			device = new ArrayList<>();
+			while (rs.next()) {
+				Stock actualStock = new Stock(rs.getInt("category_id"), rs.getString("eszkoznev"),
+						rs.getString("kelte"), rs.getString("eladas_kelte"), rs.getInt("mennyiseg"),
+						rs.getString("leiras"));
+				device.add(actualStock);
+			}
+		} catch (SQLException e) {
+			ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", e.getMessage());
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (createStatement != null) {
+					createStatement.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", e.getMessage());
+			}
+		}
+		return device;
+	}
+}
