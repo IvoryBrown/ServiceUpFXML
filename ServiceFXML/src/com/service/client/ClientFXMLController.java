c891a6ca13a1a30b91e17b87268a33d3481355d4
SzervizIvo <paradisho@gmail.com>
2018-02-22 07:08:53 +0100

    init

--- ServiceFXML/src/com/service/client/ClientFXMLController.java
+++ ServiceFXML/src/com/service/client/ClientFXMLController.java
@@ -17,10 +17,12 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
 
 public class ClientFXMLController extends MenuTreeItemController {
-	ShowInfo showInfo = new ShowInfo();
+	
 	@FXML
 	private ComboBox<String> cmbClientInputCounty;
 	@FXML
@@ -32,6 +34,8 @@ public class ClientFXMLController extends MenuTreeItemController {
 	private TextArea txtClientInputComment;
 	@FXML
 	private Button btnClientNewClient;
+	
+	private TrayNotification tray;
 
 	private final String COUNTRYCOUNTIES[] = { "Bács-Kiskun", "Baranya", "Békés", " Borsod-Abaúj-Zemplén", "Csongrád",
 			" Fejér", " Győr-Moson-Sopron", "Hajdú-Bihar", "Heves", "Jász-Nagykun-Szolnok", " Komárom-Esztergom ", "Nógrád",
@@ -40,39 +44,36 @@ public class ClientFXMLController extends MenuTreeItemController {
 	protected void setComponentAll() {
 		txtClientInputComment.setWrapText(true);
 		cmbClientInputCounty.getItems().addAll(COUNTRYCOUNTIES);
-		btnClientNewClient.setTooltip(new Tooltip(" "));
-		txtClientInputSettlement.setTooltip(new Tooltip(" EZT N�ZD WAZZEG"));
 		btnClientNewClient.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent event) {
 				btnClientNewclient();
-
 			}
 		});
 	}
 
 	private boolean clientInputBoolen() {
 		if (txtClientInputClientName.getText().trim().isEmpty()) {
-			txtClientInputClientName.getStyleClass().add("errorTextField");
+			txtClientInputClientName.setStyle("-fx-prompt-text-fill: #CC0033");
 		}
 		if (cmbClientInputCounty.getValue() == null) {
-			cmbClientInputCounty.setPromptText("K�rlek v�lasz!");
+			cmbClientInputCounty.setPromptText("Kérlek válasz!");
 		}
 		if (txtClientInputSettlement.getText().trim().isEmpty()) {
-			txtClientInputSettlement.getStyleClass().add("errorTextField");
+			txtClientInputSettlement.setStyle("-fx-prompt-text-fill: #CC0033");
 		}
 		if (txtClientInputZipCode.getText().trim().isEmpty()) {
-			txtClientInputZipCode.getStyleClass().add("errorTextField");
+			txtClientInputZipCode.setStyle("-fx-prompt-text-fill: #CC0033");
 		}
 		if (txtClientInputMobil.getText().trim().isEmpty()) {
-			txtClientInputMobil.getStyleClass().add("errorTextField");
+			txtClientInputMobil.setStyle("-fx-prompt-text-fill: #CC0033");
 		}
 		if (txtClientInputAdministrator.getText().trim().isEmpty()) {
-			txtClientInputAdministrator.getStyleClass().add("errorTextField");
+			txtClientInputAdministrator.setStyle("-fx-prompt-text-fill: #CC0033");
 		}
 		if (txtClientInputAddress.getText().trim().isEmpty()) {
-			txtClientInputAddress.getStyleClass().add("errorTextField");
+			txtClientInputAddress.setStyle("-fx-prompt-text-fill: #CC0033");
 		}
 		if (cmbClientInputCounty.getValue() == null || txtClientInputClientName.getText().trim().isEmpty()
 				|| txtClientInputSettlement.getText().trim().isEmpty()
@@ -113,8 +114,8 @@ public class ClientFXMLController extends MenuTreeItemController {
 				try {
 					insertClient.setInt(5, Integer.parseInt(txtClientInputZipCode.getText()));
 				} catch (NumberFormatException e) {
-					clientPane.setOpacity(0.1);
-					ShowInfo.errorInfoMessengeException("HIBA", "Nem megfelelő Irányítószám!", e.getMessage());
+					tray = new TrayNotification("HIBA", "Nem megfelelő irányítószám!", NotificationType.ERROR);
+					tray.showAndDismiss(Duration.seconds(2));
 				}
 				insertClient.setString(6, txtClientInputAddress.getText());
 				insertClient.setString(7, txtClientInputEmail.getText());
@@ -123,17 +124,15 @@ public class ClientFXMLController extends MenuTreeItemController {
 				insertClient.setString(10, txtClientInputComment.getText());
 				insertClient.executeUpdate();
 				setClientInputText();
-				clientPane.setOpacity(0.1);
-				ShowInfo.showInfoMessenge("Sikeres Frissítés ", "Remek! ");
-
+				tray = new TrayNotification("Remek!", "Sikeres Felvétel", NotificationType.SUCCESS);
+				tray.showAndDismiss(Duration.seconds(1));
 			} catch (SQLException ex) {
 				clientPane.setOpacity(0.1);
 				ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", ex.getMessage());
 			}
 		} else {
-			clientPane.setOpacity(0.1);
-			ShowInfo.errorInfoMessenge("HIBA", "Nincs minden mező kitöltve");
-
+			tray = new TrayNotification("HIBA", "Nincs minden mező kitöltve", NotificationType.ERROR);
+			tray.showAndDismiss(Duration.seconds(2));
 		}
 		clientPane.setOpacity(1);
 	}
