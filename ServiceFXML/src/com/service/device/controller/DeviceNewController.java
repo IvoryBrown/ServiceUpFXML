4a918e2b72a0b71aa88f8bb7117b98425b6b0f0c
SzervizIvo <paradisho@gmail.com>
2018-02-27 13:33:15 +0100

    init

--- ServiceFXML/src/com/service/device/controller/DeviceNewController.java
+++ ServiceFXML/src/com/service/device/controller/DeviceNewController.java
@@ -1,10 +1,16 @@
 package com.service.device.controller;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import com.service.client.table.ClientTable;
 import com.service.device.fillteringdb.DeviceFillteringDB;
+import com.service.setting.database.DataBaseConnect;
+import com.service.setting.identification.DeviceIdentificationGenereator;
+import com.service.setting.showinfo.ShowInfo;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -82,22 +88,87 @@ public class DeviceNewController extends ClientTable implements Initializable {
 
 	@FXML
 	private void setButtonNewDevie() {
-		if (setDeviceClientCheck()) {
-			if (setDeviceCheck()) {
-				if (setDeviceSoftverCheck()) {
+//		if (setDeviceClientCheck()) {
+//			if (setDeviceCheck()) {
+//				if (setDeviceSoftverCheck()) {
+					String SQL = "INSERT INTO gepadatok(ugyfel_adatok_id_ugyfel, eszkoz_azonosito, ceg_nev_gep, ugyfél_nev_gep,"
+							+ " eszkoz, eszkoz_gyarto, eszkoz_gyari_szama, javitas_helye, allapot, uj_gep, ugyintezo, prioritas,"
+							+ "jelszo, hivatkozasi_szam, tartozekok, serules, hiba_leirasa, eszkoz_megjegyzes, vasarlasi_datuma,"
+							+ " bejelentes_datuma, hatarido_datuma, kiszallas_datuma, adatmentes, softver, operacios_rendszer, "
+							+ "softver_megjegyzés, laptop, haz, tapegyseg, processzor, alaplap, memoria, videokartya, ssd, meghajto,"
+							+ " hutoventilator, optikai_meghajto, bovitokartya)"
+							+ "values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ";
+					try {
+						Connection con = DataBaseConnect.getConnection();
+						PreparedStatement insertDevice = con.prepareStatement(SQL);
 
-				} else {
-					tray = new TrayNotification("HIBA", "Nincs Softver kiválasztva", NotificationType.ERROR);
-					tray.showAndDismiss(Duration.seconds(2));
-				}
-			} else {
-				tray = new TrayNotification("HIBA", "Nincs minden mező kitöltve", NotificationType.ERROR);
-				tray.showAndDismiss(Duration.seconds(2));
-			}
-		} else {
-			tray = new TrayNotification("HIBA", "Nincs ügyfél kiválasztva", NotificationType.ERROR);
-			tray.showAndDismiss(Duration.seconds(2));
-		}
+						deviceNumber.setText(DeviceIdentificationGenereator.random());
+						insertDevice.setString(1, deviceClientId.getText());
+						insertDevice.setString(2, deviceNumber.getText());
+						insertDevice.setString(3, deviceCompanyName.getText());
+						insertDevice.setString(4, deviceClientName.getText());
+						insertDevice.setString(5, deviceName.getSelectionModel().getSelectedItem());
+						insertDevice.setString(6, deviceManufacturer.getText());
+						insertDevice.setString(7, deviceSerialNumber.getText());
+						insertDevice.setString(8, deviceRepairLocation.getSelectionModel().getSelectedItem());
+						insertDevice.setString(9, deviceStatus.getSelectionModel().getSelectedItem());
+						insertDevice.setString(10, deviceNewMachine.getSelectionModel().getSelectedItem());
+						insertDevice.setString(11, deviceAdministrator.getSelectionModel().getSelectedItem());
+						insertDevice.setString(12, devicePriorit.getSelectionModel().getSelectedItem());
+						insertDevice.setString(13, devicePassword.getText());
+						insertDevice.setString(14, deviceReferences.getText());
+						insertDevice.setString(15, deviceAccesssory.getText());
+						insertDevice.setString(16, deviceInjury.getText());
+						insertDevice.setString(17, deviceErrorDescription.getText());
+						insertDevice.setString(18, deviceComment.getText());
+						if (deviceSalesBuying.getEditor().getText()!= null) {
+							insertDevice.setString(19, ((TextField) deviceSalesBuying.getEditor()).getText());
+						} else {
+							insertDevice.setDate(19, null);
+						}
+						insertDevice.setString(20, ((TextField) deviceAddDate.getEditor()).getText());
+						insertDevice.setString(21, ((TextField) deviceEndDate.getEditor()).getText());
+						if (deviceDeliveryDate.getEditor().getText() != null) {
+							insertDevice.setString(22, ((TextField) deviceDeliveryDate.getEditor()).getText());
+						} else {
+							insertDevice.setDate(22, null);
+						}
+						insertDevice.setString(23, deviceDataRecovery.getSelectionModel().getSelectedItem());
+						insertDevice.setString(24, deviceSoftver.getSelectionModel().getSelectedItem());
+						insertDevice.setString(25, deviceOperatingSystem.getSelectionModel().getSelectedItem());
+						insertDevice.setString(26, deviceSoftverComment.getText());
+						insertDevice.setBoolean(27, deviceLaptop.isSelected());
+						insertDevice.setBoolean(28, deviceNewHouse.isSelected());
+						insertDevice.setBoolean(29, devicePowerSupply.isSelected());
+						insertDevice.setBoolean(30, deviceProcessor.isSelected());
+						insertDevice.setBoolean(31, deviceBaseBoard.isSelected());
+						insertDevice.setBoolean(32, deviceMemory.isSelected());
+						insertDevice.setBoolean(33, deviceVideoCard.isSelected());
+						insertDevice.setBoolean(34, deviceSSDDrive.isSelected());
+						insertDevice.setBoolean(35, deviceHardDrive.isSelected());
+						insertDevice.setBoolean(36, deviceCoolingFan.isSelected());
+						insertDevice.setBoolean(37, deviceOpticalDrive.isSelected());
+						insertDevice.setBoolean(38, deviceExpansionCard.isSelected());
+						insertDevice.executeUpdate();
+						setClearAllText();
+						tray = new TrayNotification("Remek!", "Sikeres Felvétel", NotificationType.SUCCESS);
+						tray.showAndDismiss(Duration.seconds(1));
+					} catch (SQLException ex) {
+						System.out.println(ex);
+						ShowInfo.errorInfoMessengeException("Adatbázis Hiba", "Szerver válasza: ", ex.getMessage());
+					}
+//				} else {
+//					tray = new TrayNotification("HIBA", "Nincs Softver kiválasztva", NotificationType.ERROR);
+//					tray.showAndDismiss(Duration.seconds(2));
+//				}
+//			} else {
+//				tray = new TrayNotification("HIBA", "Nincs minden mező kitöltve", NotificationType.ERROR);
+//				tray.showAndDismiss(Duration.seconds(2));
+//			}
+//		} else {
+//			tray = new TrayNotification("HIBA", "Nincs ügyfél kiválasztva", NotificationType.ERROR);
+//			tray.showAndDismiss(Duration.seconds(2));
+//		}
 	}
 
 	private boolean setDeviceClientCheck() {
@@ -137,20 +208,20 @@ public class DeviceNewController extends ClientTable implements Initializable {
 			deviceDataRecovery.setPromptText("Kérlek válasz!");
 			return false;
 		} else {
-			deviceName.setPromptText(null);
-			deviceManufacturer.setPromptText(null);
-			deviceSerialNumber.setPromptText(null);
-			deviceRepairLocation.setPromptText(null);
-			deviceStatus.setPromptText(null);
-			deviceNewMachine.setPromptText(null);
-			deviceAdministrator.setPromptText(null);
-			devicePriorit.setPromptText(null);
-			deviceAccesssory.setPromptText(null);
-			deviceInjury.setPromptText(null);
-			deviceErrorDescription.setPromptText(null);
-			deviceAddDate.setPromptText(null);
-			deviceEndDate.setPromptText(null);
-			deviceDataRecovery.setPromptText(null);
+			deviceName.setStyle(null);
+			deviceManufacturer.setStyle(null);
+			deviceSerialNumber.setStyle(null);
+			deviceRepairLocation.setStyle(null);
+			deviceStatus.setStyle(null);
+			deviceNewMachine.setStyle(null);
+			deviceAdministrator.setStyle(null);
+			devicePriorit.setStyle(null);
+			deviceAccesssory.setStyle(null);
+			deviceInjury.setStyle(null);
+			deviceErrorDescription.setStyle(null);
+			deviceAddDate.setStyle(null);
+			deviceEndDate.setStyle(null);
+			deviceDataRecovery.setStyle(null);
 			return true;
 		}
 
@@ -162,15 +233,59 @@ public class DeviceNewController extends ClientTable implements Initializable {
 				deviceOperatingSystem.setPromptText("Kérlek válasz!");
 				return false;
 			} else {
-				deviceOperatingSystem.setPromptText(null);
+				deviceOperatingSystem.setStyle(null);
 				return true;
 			}
 		} else {
-			deviceOperatingSystem.setPromptText(null);
+			deviceOperatingSystem.setStyle(null);
 			return true;
 		}
 	}
 
+	private void setClearAllText() {
+	 deviceClientId.clear();
+		deviceNumber.clear();
+		 deviceCompanyName.clear();
+		deviceClientName.clear();
+//		deviceName.setSelectedIndex(null);
+		deviceManufacturer.clear();
+		 deviceSerialNumber.clear();
+//		 deviceRepairLocation.getSelectionModel().getSelectedItem());
+//		 deviceStatus.getSelectionModel().getSelectedItem());
+//		 deviceNewMachine.getSelectionModel().getSelectedItem());
+//		 deviceAdministrator.getSelectionModel().getSelectedItem());
+//		 devicePriorit.getSelectionModel().getSelectedItem());
+		 devicePassword.clear();
+		 deviceReferences.clear();
+		deviceAccesssory.clear();
+		 deviceInjury.clear();
+		deviceErrorDescription.clear();
+		 deviceComment.clear();
+//		deviceSalesBuying.getEditor().setText(null);
+//		
+//		 deviceAddDate.getEditor().setText(null);
+//		 deviceEndDate.getEditor().setText(null);
+//		deviceDeliveryDate.getEditor().setText(null);
+		
+//		 deviceDataRecovery.getSelectionModel().getSelectedItem());
+//		 deviceSoftver.getSelectionModel().getSelectedItem());
+//		 deviceOperatingSystem.getSelectionModel().getSelectedItem());
+//		 deviceSoftverComment.setText(null);
+//		 deviceLaptop.isSelected());
+//		deviceNewHouse.isSelected());
+//		devicePowerSupply.isSelected());
+//		 deviceProcessor.isSelected());
+//		 deviceBaseBoard.isSelected());
+//		 deviceMemory.isSelected());
+//		 deviceVideoCard.isSelected());
+//		 deviceSSDDrive.isSelected());
+//		 deviceHardDrive.isSelected());
+//		 deviceCoolingFan.isSelected());
+//		 deviceOpticalDrive.isSelected());
+//		 deviceExpansionCard.isSelected());
+
+	}
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		setComboxAll();
