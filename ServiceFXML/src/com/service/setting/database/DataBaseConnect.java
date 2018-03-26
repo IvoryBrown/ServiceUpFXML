54c6adcdbbe32cd1fe0ffffba8cce40c744672b2
SzervizIvo <paradisho@gmail.com>
2018-03-26 06:07:24 +0200

    init

--- ServiceFXML/src/com/service/setting/database/DataBaseConnect.java
+++ ServiceFXML/src/com/service/setting/database/DataBaseConnect.java
@@ -9,7 +9,6 @@ import com.service.setting.showinfo.ShowInfo;
 
 public class DataBaseConnect {
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-
 	static final String DB_URL = "jdbc:mysql://localhost/szerviz_up?useUnicode=true&characterEncoding=UTF-8";
 	static final String USER = "root";
 	static final String PASS = "12345";
