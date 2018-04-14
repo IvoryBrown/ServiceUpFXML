011137cb4a54b366c9b657247280ddc7f515e49f
SzervizIvo <paradisho@gmail.com>
2018-04-14 10:29:50 +0200

    init

--- ServiceFXML/src/com/service/setting/email/HTMLDataSource.java
+++ ServiceFXML/src/com/service/setting/email/HTMLDataSource.java
@@ -27,6 +27,11 @@ public class HTMLDataSource implements DataSource {
 	private static String deviceEndDate;
 	private static String deviceCompletedDate;
 	private static String deviceSoftver;
+	private static String deviceClientName;
+	private static String deviceCompanyName;
+	private static String deviceStatusz;
+	private static String deviceComment;
+	private static String deviceOperatingSystem;
 
 	public HTMLDataSource() {
 
@@ -36,12 +41,15 @@ public class HTMLDataSource implements DataSource {
 		html = htmlString;
 	}
 
-	public void setHTMLCompom(String deviceNumber, String deviceName, String deviceManufacturer,
-			String deviceRepairLocation, String deviceStatus, String deviceNewMachine, String deviceAdministrator,
-			String deviceTechnicalPerson, String devicePriorit, String deviceReferences, String deviceErrorDescription,
-			String deviceErrorCorrection, String deviceDeliveryDate, String deviceAddDate, String deviceEndDate,
-			String deviceCompletedDate, String deviceSoftver) {
+	public void setHTMLCompom(String deviceCompanyName, String deviceClientName, String deviceNumber, String deviceName,
+			String deviceManufacturer, String deviceRepairLocation, String deviceStatus, String deviceNewMachine,
+			String deviceAdministrator, String deviceTechnicalPerson, String devicePriorit, String deviceReferences,
+			String deviceStatusz, String deviceErrorDescription, String deviceErrorCorrection, String deviceComment,
+			String deviceDeliveryDate, String deviceAddDate, String deviceEndDate, String deviceCompletedDate,
+			String deviceSoftver, String deviceOperatingSystem) {
 
+		HTMLDataSource.deviceCompanyName = deviceCompanyName;
+		HTMLDataSource.deviceClientName = deviceClientName;
 		HTMLDataSource.deviceNumber = deviceNumber;
 		HTMLDataSource.deviceName = deviceName;
 		HTMLDataSource.deviceManufacturer = deviceManufacturer;
@@ -52,13 +60,16 @@ public class HTMLDataSource implements DataSource {
 		HTMLDataSource.deviceTechnicalPerson = deviceTechnicalPerson;
 		HTMLDataSource.devicePriorit = devicePriorit;
 		HTMLDataSource.deviceReferences = deviceReferences;
+		HTMLDataSource.deviceStatusz = deviceStatusz;
 		HTMLDataSource.deviceErrorDescription = deviceErrorDescription;
 		HTMLDataSource.deviceErrorCorrection = deviceErrorCorrection;
+		HTMLDataSource.deviceComment = deviceComment;
 		HTMLDataSource.deviceDeliveryDate = deviceDeliveryDate;
 		HTMLDataSource.deviceAddDate = deviceAddDate;
 		HTMLDataSource.deviceEndDate = deviceEndDate;
 		HTMLDataSource.deviceCompletedDate = deviceCompletedDate;
 		HTMLDataSource.deviceSoftver = deviceSoftver;
+		HTMLDataSource.deviceOperatingSystem = deviceOperatingSystem;
 
 	}
 
@@ -87,6 +98,12 @@ public class HTMLDataSource implements DataSource {
 				+ "<hr />" + "<p>&nbsp;</p>"
 				+ "<table style=\"height: 48px; margin-left: auto; margin-right: auto; width: 612px;\" bgcolor=\"#61a2b1\">"
 				+ "<tbody>" + "<tr>"
+				+ "<td style=\"width: 239px; text-align: right; background-color: #616161;\"><span style=\"color: whitesmoke;\">&Uuml;gyf&eacute;l:</span></td>"
+				+ "<td style=\"width: 501px; text-align: left; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
+				+ deviceClientName + "</span></td>" + "</tr>" + "<tr>"
+				+ "<td style=\"width: 239px; text-align: right; background-color: #616161;\"><span style=\"color: whitesmoke;\">C&eacute;g:</span></td>"
+				+ "<td style=\"width: 501px; text-align: left; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
+				+ deviceCompanyName + "</span></td>" + "</tr>" + "<tr>"
 				+ "<td style=\"width: 239px; text-align: right; background-color: #616161;\"><span style=\"color: whitesmoke;\">Eszk&ouml;z Azonos&iacute;t&oacute;:</span></td>"
 				+ "<td style=\"width: 501px; text-align: left; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
 				+ deviceNumber + "</span></td>" + "</tr>" + "<tr>"
@@ -117,12 +134,18 @@ public class HTMLDataSource implements DataSource {
 				+ "<td style=\"width: 239px; background-color: #424242; text-align: right;\">&nbsp;<span style=\"color: whitesmoke;\">Hivatkoz&aacute;si sz&aacute;m:</span></td>"
 				+ "<td style=\"width: 501px; background-color: #424242;\"><span style=\"color: whitesmoke;\">&nbsp;"
 				+ deviceReferences + "</span></td>" + "</tr>" + "<tr>"
+				+ "<td style=\"width: 239px; text-align: right; background-color: #616161;\"><span style=\"color: whitesmoke;\">St&aacute;tusz:</span></td>"
+				+ "<td style=\"width: 501px; text-align: left; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
+				+ deviceStatusz + "</span></td>" + "</tr>" + "<tr>"
 				+ "<td style=\"width: 239px; background-color: #616161; text-align: right;\">&nbsp;<span style=\"color: whitesmoke;\">Hiba Le&iacute;r&aacute;s:</span></td>"
 				+ "<td style=\"width: 501px; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
 				+ deviceErrorDescription + "</span></td>" + "</tr>" + "<tr>"
 				+ "<td style=\"width: 239px; background-color: #424242; text-align: right;\">&nbsp;<span style=\"color: whitesmoke;\">Val&oacute;s hiba:</span></td>"
 				+ "<td style=\"width: 501px; background-color: #424242;\"><span style=\"color: whitesmoke;\">&nbsp;"
 				+ deviceErrorCorrection + "</span></td>" + "</tr>" + "<tr>"
+				+ "<td style=\"width: 239px; text-align: right; background-color: #616161;\"><span style=\"color: whitesmoke;\">Eszk&ouml;zről megjegyz&eacute;s:</span></td>"
+				+ "<td style=\"width: 501px; text-align: left; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
+				+ deviceComment + "</span></td>" + "</tr>" + "<tr>"
 				+ "<td style=\"width: 239px; background-color: #616161; text-align: right;\">&nbsp;<span style=\"color: whitesmoke;\">Kisz&aacute;ll&aacute;s:</span></td>"
 				+ "<td style=\"width: 501px; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
 				+ deviceDeliveryDate + "</span></td>" + "</tr>" + "<tr>"
@@ -137,9 +160,12 @@ public class HTMLDataSource implements DataSource {
 				+ deviceCompletedDate + "</span></td>" + "</tr>" + "<tr>"
 				+ "<td style=\"width: 239px; background-color: #424242; text-align: right;\">&nbsp;<span style=\"color: whitesmoke;\">Szoftver:</span></td>"
 				+ "<td style=\"width: 501px; background-color: #424242;\"><span style=\"color: whitesmoke;\">&nbsp;"
-				+ deviceSoftver + "</span></td>" + "</tr>" + "</tbody>" + "</table>" + "<p>&nbsp;</p>" + "<hr />"
-				+ "<p><em>Ezt az emailt a PcVIpService k&uuml;ldte " + LocalDateTime.now() + ".&nbsp;</em></p>"
-				+ "<p><em>Erre az emailre ne &iacute;rj vissza!</em></p>" + "</html>";
+				+ deviceSoftver + "</span></td>" + "</tr>" + "<tr>"
+				+ "<td style=\"width: 239px; text-align: right; background-color: #616161;\"><span style=\"color: whitesmoke;\">Oper&aacute;ci&oacute;s rendszer:</span></td>"
+				+ "<td style=\"width: 501px; text-align: left; background-color: #616161;\"><span style=\"color: whitesmoke;\">&nbsp;"
+				+ deviceOperatingSystem + "</span></td>" + "</tr>" + "</tbody>" + "</table>" + "<p>&nbsp;</p>"
+				+ "<hr />" + "<p><em>Ezt az emailt a PcVIpService k&uuml;ldte " + LocalDateTime.now()
+				+ ".&nbsp;</em></p>" + "<p><em>Erre az emailre ne &iacute;rj vissza!</em></p>" + "</html>";
 		return html;
 
 	}
